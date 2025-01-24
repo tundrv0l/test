8307a24f75a3420103c9f37532493b89939062ab
Parker Clark <tundravol2@gmail.com>
2025-01-24 17:48:28 -0600

    Oh boy

--- backend/app.py
+++ backend/app.py
@@ -26,7 +26,7 @@ def report_issue():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
-    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
