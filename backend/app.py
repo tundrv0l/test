547d6d62ec3d7677a3fd4a4442a4551a92094a76
Parker Clark <tundravol2@gmail.com>
2025-01-24 15:39:16 -0600

    Testing

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,6 @@
 
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
-import os
 
 app = Flask(__name__, static_folder='../my-react-app/build', static_url_path='/')
 CORS(app)
